884b62cc02abedb4b8420aef0bf9298d64516b7b
Paul <247144100+paulperrin-stack@users.noreply.github.com>
2026-03-10 11:22:28 +0700

    Homework completed on 10 Mar.

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,16 @@
-const permutations = function() {
-  
+const permutations = function(array, index = 0, results = []) {
+    if (index == array.length) {
+        results.push([...array]);
+        return results;
+    }
+
+    for (let i = index; i < array.length; i++) {
+        [array[index], array[i]] = [array[i], array[index]];
+        permutations(array, index + 1, results);
+        [array[index], array [i]] = [array[i], array[index]];
+    }
+
+    return results;
 };
   
 // Do not edit below this line
--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,16 @@
-const pascal = function() {
-  
+const pascal = function(counter) {
+    const currentLine = [1];
+    if (counter === 1) {
+        return currentLine;
+    }
+
+    const previousLine = pascal(counter - 1);
+    previousLine.forEach((number, i) => {
+        const rightNeighbor = previousLine[i + 1] ?? 0;
+        currentLine.push(number + rightNeighbor);
+    })
+
+    return currentLine;
 };
   
 // Do not edit below this line
